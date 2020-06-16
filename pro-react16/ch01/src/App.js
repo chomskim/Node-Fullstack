919734537ca8550c084c6999af7aadbefc96d193
chomskim <csknova@gmail.com>
2020-06-16 09:07:07 +0900

    ch01 final

--- pro-react16/ch01/src/App.js
+++ pro-react16/ch01/src/App.js
@@ -21,14 +21,13 @@ export default class App extends Component {
     this.setState({ newItemText: event.target.value });
   }
 
-  createNewTodo = () => {
-    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
-      this.setState({
-        todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }],
-        //newItemText: ''
-      });
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
+        this.setState({ 
+            todoItems: [...this.state.todoItems, { action: task, done: false }]
+        }, () => localStorage.setItem("todos", JSON.stringify(this.state)));        
     }
-  }
+}
 
   toggleTodo = (todo) => this.setState({
     todoItems:
@@ -39,6 +38,20 @@ export default class App extends Component {
   todoTableRows = (doneValue) => this.state.todoItems
     .filter(item => item.done === doneValue).map(item =>
       <TodoRow key={item.action} item={item} callback={this.toggleTodo} />)
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+      ? JSON.parse(data)
+      : {
+        userName: "Adam",
+        todoItems: [{ action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }],
+        showCompleted: true
+      });
+  }
 
   render() {
     return (
