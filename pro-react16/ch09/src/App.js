8bacdc64185bb04cba18e9d761660b19a3d218c1
chomskim <csknova@gmail.com>
2020-06-18 18:30:05 +0900

    ch09 final

--- pro-react16/ch09/src/App.js
+++ pro-react16/ch09/src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
-import reactLogo from "./logo.svg";
+import { Display } from "./Display";
 
-let name = "Adam";
-const city = "London";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "London"
+    }
+  }
 
-export default class extends Component {
-  message = () => `Hello ${name} from ${city}`;
+  changeCity = () => {
+    // debugger
+    this.setState({ city: this.state.city === "London" ? "New York" : "London" })
+  }
 
   render = () => (
-    <div className="text-center">
-      <h4 className="bg-primary text-white text-center p-3">
-        {this.message()}
-      </h4>
-      <img src={reactLogo} alt="reactLogo" />
-      <link rel="stylesheet"
-        href={process.env.PUBLIC_URL + "/static.css"} />
-      <img src={process.env.PUBLIC_URL + "/logo.svg"} alt="reactLogo" />
-    </div>
+    <Display value={this.state.city} callback={this.changeCity} />
   );
   // React.createElement("h4",
   // { className: "bg-primary text-white text-center p-3" },
