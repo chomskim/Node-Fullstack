58b6efc282937e762bb2f677d62edbb209d8c5bb
chomskim <csknova@gmail.com>
2020-06-19 10:14:55 +0900

    ch12 final

--- pro-react16/ch12/src/App.js
+++ pro-react16/ch12/src/App.js
@@ -9,7 +9,6 @@ export default class App extends Component {
       counter: 0,
       theme: "secondary",
     }
-    this.handleEvent = this.handleEvent.bind(this);
   }
 
   selectTheme = (newTheme) => {
@@ -19,45 +18,21 @@ export default class App extends Component {
     });
   }
 
-  //handleEvent = () => {
-  //  console.log("handleEvent method invoked");
-  //  this.setState({ message: "Clicked!" });
-  //}
-
-  handleEvent(event) {
-    event.persist();
-    this.setState({
-      counter: this.state.counter + 1,
-      theme: event.target.innerText === "Normal" ? "primary" : "danger",
-    }, () => this.setState({ message: `${event.type}: ${this.state.counter}` }));
-  }
-
-  toggleCheckBox = (event) => {
-    if (this.state.counter === 0) {
-      event.preventDefault();
-    }
+  handleClick = (event) => {
+    console.log(`App: Type: ${event.type} `
+      + `Target: ${event.target.tagName} `
+      + `CurrentTarget: ${event.currentTarget.tagName}`);
   }
 
   render() {
     return (
-      <div className="m-2">
-        <div className="form-check">
-          <input className="form-check-input" type="checkbox"
-            onClick={this.toggleCheckBox} />
-          <label>This is a checkbox</label>
-        </div>
+      <div className="m-2" onClick={ this.handleClick }>
         <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
           {this.state.message}
         </div>
-        <div className="text-center">
-          <button className="btn btn-primary"
-            onClick={this.handleEvent} >
-            Normal
-          </button>
-          <button className="btn btn-danger m-1"
-            onClick={this.handleEvent} >
-            Danger
-          </button>
+        <div className="text-center" onClick={ this.handleClick }>
+          <ThemeButton theme="primary" callback={this.selectTheme} />
+          <ThemeButton theme="danger" callback={this.selectTheme} />
         </div>
       </div>
     )
