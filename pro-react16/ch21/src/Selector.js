f814009b7036e119cd5ab3f01de49bccb647188a
chomskim <csknova@gmail.com>
2020-06-21 17:45:50 +0900

    ch21 fin

--- pro-react16/ch21/src/Selector.js
+++ pro-react16/ch21/src/Selector.js
@@ -1,30 +1,34 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { HashRouter as Router, NavLink, Route, Switch, Redirect } from "react-router-dom";
 
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 
 export class Selector extends Component {
+  renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{msg}</h5>
+
   render() {
     return (
       <Router>
         <div className="container-fluid">
           <div className="row">
             <div className="col-2">
-              <div><Link to="/products">Products</Link></div>
-              <div><Link to="/suppliers">Suppliers</Link></div>
+              <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active" exact={ true }
+                to="/">Default URL</NavLink>
+              <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active"
+                to="/products">Products</NavLink>
+              <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active"
+                to="/suppliers">Suppliers</NavLink>
+              <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active"
+                to="/old/data">Old Link</NavLink>            
             </div>
             <div className="col">
-              <Route path="/products" render={(routeProps) =>
-                <ProductDisplay myProp="myValue" />} />
-              <Route path="/suppliers" render={(routeProps) =>
-                <React.Fragment>
-                  <h4 className="bg-info text-center text-white p-2">
-                    Suppliers
-                  </h4>
-                  <SupplierDisplay />
-                </React.Fragment>
-              } />
+              <Switch>
+                <Route path="/products" component={ProductDisplay} />
+                <Route path="/suppliers" component={SupplierDisplay} />
+                <Redirect from="/old/data" to="/suppliers" />
+                <Redirect to="/products" />
+              </Switch>
             </div>
           </div>
         </div>
