70fb883b222b9a17e528c1cfaad0a58bd2bd2586
chomskim <csknova@gmail.com>
2020-06-21 12:08:11 +0900

    ch19 fin

--- pro-react16/ch19/src/SupplierDisplay.js
+++ pro-react16/ch19/src/SupplierDisplay.js
@@ -2,69 +2,69 @@ import React, { Component } from "react";
 import { SupplierEditor } from "./SupplierEditor";
 import { SupplierTable } from "./SupplierTable";
 import { connect } from "react-redux";
-import { saveSupplier, deleteSupplier} from "./store";
+import { startCreatingSupplier } from "./store/stateActions";
+import { SUPPLIERS } from "./store/dataTypes";
+import { EditorConnector } from "./store/EditorConnector";
+import { TableConnector } from "./store/TableConnector";
+
+const ConnectedEditor = EditorConnector(SUPPLIERS, SupplierEditor);
+const ConnectedTable = TableConnector(SUPPLIERS, SupplierTable);
 
 const mapStateToProps = (storeData) => ({
-    suppliers: storeData.suppliers
+  editing: storeData.stateData.editing,
+  selected: storeData.modelData.suppliers
+    .find(item => item.id === storeData.stateData.selectedId) || {},
 })
 
 const mapDispatchToProps = {
-    saveCallback: saveSupplier,
-    deleteCallback: deleteSupplier
+  createSupplier: startCreatingSupplier,
 }
 
 const connectFunction = connect(mapStateToProps, mapDispatchToProps);
 
 export const SupplierDisplay = connectFunction(
-    class extends Component {
+  class extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      showEditor: false,
-      selected: null
+    //constructor(props) {
+    //  super(props);
+    //  this.state = {
+    //    showEditor: false,
+    //    selected: null
+    //  }
+    //}
+    /*
+    startEditing = (supplier) => {
+      this.setState({ showEditor: true, selected: supplier })
     }
-  }
 
-  startEditing = (supplier) => {
-    this.setState({ showEditor: true, selected: supplier })
-  }
+    createSupplier = () => {
+      this.setState({ showEditor: true, selected: {} })
+    }
 
-  createSupplier = () => {
-    this.setState({ showEditor: true, selected: {} })
-  }
+    cancelEditing = () => {
+      this.setState({ showEditor: false, selected: null })
+    }
 
-  cancelEditing = () => {
-    this.setState({ showEditor: false, selected: null })
-  }
-
-  saveSupplier = (supplier) => {
-    this.props.saveCallback(supplier);
-    this.setState({ showEditor: false, selected: null })
-  }
-
-  render() {
-    if (this.state.showEditor) {
-      return <SupplierEditor
-        key={this.state.selected.id || -1}
-        supplier={this.state.selected}
-        saveCallback={this.saveSupplier}
-        cancelCallback={this.cancelEditing} />
-    } else {
-      return (
-        <div className="m-2">
-          <SupplierTable suppliers={this.props.suppliers}
-            editCallback={this.startEditing}
-            deleteCallback={this.props.deleteCallback}
-          />
-          <div className="text-center">
-            <button className="btn btn-primary m-1"
-              onClick={this.createSupplier}>
-              Create Supplier
-          </button>
+    saveSupplier = (supplier) => {
+      this.props.saveCallback(supplier);
+      this.setState({ showEditor: false, selected: null })
+    }
+    */
+    render() {
+      if (this.props.editing) {
+        return <ConnectedEditor key={this.props.selected.id || -1} />
+      } else {
+        return (
+          <div className="m-2">
+            <ConnectedTable />
+            <div className="text-center">
+              <button className="btn btn-primary m-1"
+                onClick={this.props.createSupplier}>
+                Create Supplier
+            </button>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
-  }
-})
+  })
--- pro-react16/ch19/src/store/index.js
+++ pro-react16/ch19/src/store/index.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import modelReducer from "./modelReducer";
+import stateReducer from "./stateReducer";
 
-export default createStore(modelReducer);
+export default createStore(combineReducers(
+  {
+    modelData: modelReducer,
+    stateData: stateReducer
+  }));
 
 export { saveProduct, saveSupplier, deleteProduct, deleteSupplier }
   from "./modelActionCreators";
