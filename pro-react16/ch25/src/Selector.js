1dbaeacd8f59769b1588ebf9d9cfb63473a153f9
chomskim <csknova@gmail.com>
2020-06-22 23:42:41 +0900

    ch25 fin

---
+++ pro-react16/ch25/src/Selector.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+
+import { ToggleLink } from "./routing/ToggleLink";
+// import { RoutedDisplay } from "./routing/RoutedDisplay";
+// import { IsolatedTable } from "./IsolatedTable";
+// import { IsolatedEditor } from "./IsolatedEditor";
+// import { RequestError } from "./webservice/RequestError";
+import { GraphQLTable } from "./graphql/GraphQLTable";
+import { PRODUCTS, SUPPLIERS } from "./store/dataTypes";
+import { GraphQLEditor } from "./graphql/GraphQLEditor";
+
+export class Selector extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2">
+              <ToggleLink to="/products">Products</ToggleLink>
+              <ToggleLink to="/suppliers">Suppliers</ToggleLink>
+            </div>
+            <div className="col">
+              <Switch>
+                <Route path="/products" exact={true}
+                  component={GraphQLTable(PRODUCTS)} />
+                <Route path="/suppliers" exact={true}
+                  component={GraphQLTable(SUPPLIERS)} />
+                <Route path="/:dataType/edit/:id"
+                  component={GraphQLEditor()} />
+                <Redirect to="/products" />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
