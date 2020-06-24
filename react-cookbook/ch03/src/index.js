4e425001c66fe5a7c9de0145783eb0154177b3f1
chomskim <csknova@gmail.com>
2020-06-24 18:26:27 +0900

    ch03 fin

--- react-cookbook/ch03/src/index.js
+++ react-cookbook/ch03/src/index.js
@@ -1,6 +1,7 @@
+/* eslint react/jsx-filename-extension: "off" */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/Calculator/App';
+import App from './components/Person/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
