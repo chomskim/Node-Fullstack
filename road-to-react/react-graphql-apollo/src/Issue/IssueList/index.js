79d2e5fadb8e84a5e7db0b35d761b89bff8df19c
chomskim <csknova@gmail.com>
2020-06-29 09:01:55 +0900

    react-graphql-apollo filter

---
+++ road-to-react/react-graphql-apollo/src/Issue/IssueList/index.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import IssueItem from '../IssueItem';
+import Loading from '../../Loading';
+import ErrorMessage from '../../Error';
+import { ButtonUnobtrusive } from '../../Button';
+
+import './style.css';
+
+const GET_ISSUES_OF_REPOSITORY = gql`
+  query($repositoryOwner: String!, $repositoryName: String!) {
+    repository(name: $repositoryName, owner: $repositoryOwner) {
+      issues(first: 5) {
+        edges {
+          node {
+            id
+            number
+            state
+            title
+            url
+            bodyHTML
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ISSUE_STATES = {
+  NONE: 'NONE',
+  OPEN: 'OPEN',
+  CLOSED: 'CLOSED',
+};
+
+const isShow = issueState => issueState !== ISSUE_STATES.NONE;
+
+const TRANSITION_LABELS = {
+  [ISSUE_STATES.NONE]: 'Show Open Issues',
+  [ISSUE_STATES.OPEN]: 'Show Closed Issues',
+  [ISSUE_STATES.CLOSED]: 'Hide Issues',
+};
+
+const TRANSITION_STATE = {
+  [ISSUE_STATES.NONE]: ISSUE_STATES.OPEN,
+  [ISSUE_STATES.OPEN]: ISSUE_STATES.CLOSED,
+  [ISSUE_STATES.CLOSED]: ISSUE_STATES.NONE,
+};
+
+class Issues extends React.Component {
+  state = {
+    issueState: ISSUE_STATES.NONE,
+  };
+
+  onChangeIssueState = nextIssueState => {
+    this.setState({ issueState: nextIssueState });
+  };
+
+  render() {
+    const { issueState } = this.state;
+    const { repositoryOwner, repositoryName } = this.props;
+
+    return (
+    <div className="Issues">
+      <ButtonUnobtrusive
+        onClick={() =>
+          this.onChangeIssueState(TRANSITION_STATE[issueState])
+        }
+      >
+        {TRANSITION_LABELS[issueState]}
+      </ButtonUnobtrusive>
+
+      {isShow(issueState) && (
+        <Query
+          query={GET_ISSUES_OF_REPOSITORY}
+          variables={{
+            repositoryOwner,
+            repositoryName,
+          }}
+        >
+          {({ data, loading, error }) => {
+            if (error) {
+              return <ErrorMessage error={error} />;
+            }
+
+            const { repository } = data;
+
+            if (loading && !repository) {
+              return <Loading />;
+            }
+
+            const filteredRepository = {
+              issues: {
+                edges: repository.issues.edges.filter(
+                  issue => issue.node.state === issueState,
+                ),
+              },
+            };
+
+            if (!filteredRepository.issues.edges.length) {
+              return <div className="IssueList">No issues ...</div>;
+            }
+
+            return (
+              <IssueList issues={filteredRepository.issues} />
+            );
+          }}
+        </Query>
+      )}
+    </div>
+    );
+  }
+}
+
+
+const IssueList = ({ issues }) => (
+  <div className="IssueList">
+    {issues.edges.map(({ node }) => (
+      <IssueItem key={node.id} issue={node} />
+    ))}
+  </div>
+);
+
+export default Issues;
