547a1e459cbcbe85ae7ddd84c654618301877ccb
chomskim <csknova@gmail.com>
2020-07-04 21:06:26 +0900

    fullstack-apollo-express-postgresql init

---
+++ road-to-react/fullstack-apollo-express-postgresql/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
