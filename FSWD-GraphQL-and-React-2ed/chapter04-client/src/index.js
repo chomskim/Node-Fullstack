c74ca487a70158c414874a8aacf5304e8d10cf79
chomskim <csknova@gmail.com>
2022-07-04 10:50:24 +0900

    update cra to client

---
+++ FSWD-GraphQL-and-React-2ed/chapter04-client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { ApolloProvider } from '@apollo/client/react';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import client from './apollo';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
